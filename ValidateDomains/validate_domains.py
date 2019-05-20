831efc780cfd255a775484c79fec83e3acddecc2
thoward-gis <47043844+thoward-gis@users.noreply.github.com>
2019-05-20 09:40:44 -0600

    Update validate_domains.py
    - Added second parameter named null_check.
    - Added ignore null values option to validate_domains() function.
    - Bug fix, added sys package import.

--- ValidateDomains/validate_domains.py
+++ ValidateDomains/validate_domains.py
@@ -1,7 +1,7 @@
 """
 Tool Name: Validate Domain Values
-Source: domain_valitdate.py
-Last Updated: May 14, 2019
+Source: domain_validate.py
+Last Updated: May 20, 2019
 Version: ArcGIS 10+ or ArcGIS Pro 1.0+
 
 Required Arguments:
@@ -12,12 +12,13 @@ If errors are found, an ERROR_MESSAGE field is added to the input feature class 
 """
 import arcpy
 import os
+import sys
 import traceback
 
 
 def dict_of_domains(fc):
     """
-    create dictionary from domains to use to check attribute values
+    create dictionary from database domains to use to check attribute values
     """
     # need to find root database (GDB or SDE)
     db_root = os.path.dirname(fc)
@@ -30,7 +31,7 @@ def dict_of_domains(fc):
     return {domain.name: domain.codedValues for domain in arcpy.da.ListDomains(db_root)}
 
 
-def validate_domains(fc, domain_dict):
+def validate_domains(fc, domain_dict, null_ignore=False):
     """ check all attribute values against domain value lists
 
     :param fc: Input feature class to check
@@ -43,23 +44,29 @@ def validate_domains(fc, domain_dict):
         arcpy.AddField_management(fc, "ERROR_MESSAGE", field_type="TEXT", field_length=1000)
         list_of_field_names.append("ERROR_MESSAGE")
 
-    # for field in list_of_fields:
-    #     # check if field has domain
-    #     if field.domain:
+    # parse through all records and fields and insert error message if error found
     with arcpy.da.UpdateCursor(fc, list_of_field_names) as cur:
         for row in cur:
             error_message = ''
             for idx in range(len(row)-1):
                 # check if field has domain
                 if list_of_fields[idx].domain:
+                    # check if field value is in domain value list
                     if row[idx] not in domain_dict[list_of_fields[idx].domain].keys():
-                        error_message += str(list_of_field_names[idx]) + " contains invalid value - " + str(row[idx]) + ", "
+                        if not null_ignore:
+                            error_message += "{} contains invalid value - {}, ".format(str(list_of_field_names[idx]), str(row[idx]))
+                        else:
+                            # ignore nulls when creating error message
+                            if row[idx]:
+                                error_message += "{} contains invalid value - {}, ".format(
+                                    str(list_of_field_names[idx]), str(row[idx]))
             if error_message:
                 row[list_of_field_names.index("ERROR_MESSAGE")] = error_message[:-2]  # drop last comma
             else:
+                # set error message field to null if there are no errors
                 row[list_of_field_names.index("ERROR_MESSAGE")] = None
             cur.updateRow(row)
-        del cur  # get rid of any remaining locks
+    del cur  # get rid of any remaining locks
 
 
 def del_error_field(fc, field="ERROR_MESSAGE"):
@@ -71,7 +78,7 @@ def del_error_field(fc, field="ERROR_MESSAGE"):
         arcpy.DeleteField_management(fc, field)
         arcpy.AddMessage("No Domain Errors found! ERROR_MESSAGE field has been removed.")
     else:
-        arcpy.AddMessage("Domain Errors Found! See ERROR_MESSAGE field added to input.")
+        arcpy.AddWarning("Domain Errors Found! See ERROR_MESSAGE field added to input.")
 
 
 #####################################################
@@ -81,12 +88,17 @@ if __name__ == '__main__':
     try:
         # Get parameters
         in_fc = arcpy.GetParameterAsText(0)
+        null_check = arcpy.GetParameter(1)
         # create dictionary of domains
         domain_dic = dict_of_domains(in_fc)
         # arcpy.AddMessage(domain_dic)
         # validate domains if domains exist
         if domain_dic:
-            validate_domains(in_fc, domain_dic)
+            # ignore nulls if box checked
+            if null_check:
+                validate_domains(in_fc, domain_dic, True)
+            else:
+                validate_domains(in_fc, domain_dic)
             # delete ERROR_MESSAGE field if no problems found
             del_error_field(in_fc)
         else:
